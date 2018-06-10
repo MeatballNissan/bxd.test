514efd241f52f633bf9e884b16400c40f9429509
bxd <8981540@qq.com>
2018-06-10 17:57:07 +0800

    default commit
    Signed-off-by: bxd <8981540@qq.com>

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+/**
+ * Created by PC on 2018/6/7.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class A {
+    private String a;
+}
